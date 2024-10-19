d4cbfe19b95a9cb545df429e0fa6f475665b97d5
Grzegorz Kmita <grzegorz.kmita@zendesk.com>
2024-10-19 23:48:25 +0200

    Initial commit

---
+++ database/events.sql
@@ -0,0 +1,10 @@
+-- Sample events
+INSERT INTO events (title, start_date, end_date, description, image_path, category_id, user_id) VALUES
+('Spotkanie zespołu', '2024-01-15', '2024-01-15', 'Spotkanie zespołu w celu omówienia postępów projektów.', 'meeting.png', 1, 2),
+('Uroczystość rocznicowa', '2024-02-20', '2024-02-20', 'Uroczystość z okazji 10-lecia firmy.', 'anniversary.png', 2, 1),
+('Warsztaty kreatywne', '2024-03-05', '2024-03-05', 'Warsztaty dla pracowników w celu rozwijania umiejętności.', 'workshop.png', 1, 3),
+('Spotkanie integracyjne', '2024-04-10', '2024-04-10', 'Spotkanie integracyjne dla nowych pracowników.', 'integration.png', 1, 1),
+('Szkolenie z zarządzania czasem', '2024-05-15', '2024-05-15', 'Szkolenie dotyczące efektywnego zarządzania czasem.', 'time_management.png', 4, 1),
+('Impreza letnia', '2024-06-20', '2024-06-20', 'Letnia impreza dla pracowników i ich rodzin.', 'summer_party.png', 5, 1),
+('Spotkanie z klientem', '2024-07-05', '2024-07-05', 'Spotkanie z kluczowym klientem w celu omówienia współpracy.', 'client_meeting.png', 1, 2),
+('Zebranie roczne', '2024-08-30', '2024-08-30', 'Zebranie roczne podsumowujące osiągnięcia firmy.', 'annual_meeting.png', 1, 3);
---
+++ database/users.sql
@@ -0,0 +1,5 @@
+-- Sample users
+INSERT INTO users (username, password_hash) VALUES
+('admin', '$2y$10$ZnA7gTHPvc7OEHKW1J/WrOR5rZTiAKVnHSW7T64.O/5USvlCE85iC'), -- hasło: p@$$vv0rd
+('tomek', '$2y$10$0EvTO2U7iqOCCbsIwERDLuc1o7d2i8OkIFdg4Lmm.EBzKjjpsiDze'), -- hasło: marysia_jest_super!
+('marysia', '$2y$10$LMvsOky7EMT7cDgBtDETqeaAIbVgb5t2.YMcuX.4J6Ao4yqZcBD..'); -- hasło: nie_lubie_tomka
